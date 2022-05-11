19cb2e56585f1e0bd05bae2cdfd1aa9bda7c1caf
Mychell Suesca <mychell.0628@gmail.com>
2022-05-11 12:06:39 -0500

    Forma 2

--- src/Clases/Forma2.java
+++ src/Clases/Forma2.java
@@ -2,4 +2,38 @@ package Clases;
 
 public class Forma2 {
 
+    //Atributos
+    private int du,n,vf2[];
+
+    public Forma2(int du, int n, int[] vf2) {
+        du = (vf2[0]*2)+2;
+        this.n = n;
+        vf2 = new int[n];
+    }
+
+    public int getDu() {
+        return du;
+    }
+
+    public void setDu(int du) {
+        this.du = du;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int[] getVf2() {
+        return vf2;
+    }
+
+    public void setVf2(int[] vf2) {
+        this.vf2 = vf2;
+    }
+    
+    
 }
