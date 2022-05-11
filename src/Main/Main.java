63c8563380bbbbe8c2e7dbfa42a050bc20a7436b
Mychell Suesca <mychell.0628@gmail.com>
2022-05-11 00:44:37 -0500

    Insertar Eliminar Terminos

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,12 +2,16 @@ package Main;
 
 import javax.swing.JOptionPane;
 
+import Clases.Forma1;
+import Clases.Forma2;
+
 public class Main {
 
 	public static void main(String[] args) {
 		String[] result = null; 
 		String subMenu = null; 
 		String res = null;
+		String termino = null;
         result = Menu();
             switch(result[0]){
                 case "1":
@@ -15,24 +19,82 @@ public class Main {
                               "1.) Insertar Término \n"+
                 			  "2.) Eliminar Término");
                 	InsertarEliminarTerminos iet = new InsertarEliminarTerminos();
-                	String termino = JOptionPane.showInputDialog("Ingrese el término:\n");
-                	iet.setTermino(termino);
-                	iet.setPolinomio(result[1]);
-                	System.out.println(subMenu);
                 	switch(subMenu){
                 		case "1":
+                			termino = JOptionPane.showInputDialog("Ingrese el término a "+ (subMenu == "1" ? "insertar" : "eliminar"));
+                        	iet.setTermino(termino);
+                        	iet.setPolinomio(result[1]);
                 			res = iet.insertarTermino();
                 		break;
                 		case "2":
-                			iet.eliminarTermino();
+                			termino = JOptionPane.showInputDialog("Ingrese el término a "+ (subMenu == "1" ? "insertar" : "eliminar"));
+                        	iet.setTermino(termino);
+                        	iet.setPolinomio(result[1]);
+                			res = iet.eliminarTermino();
                 		break;
+                		default:
+                			JOptionPane.showMessageDialog(null, "Opción no válida", "Proyecto Polinomios", 0);
+                        break;
                 	}
                 break;
                 case "2":
+                	subMenu = JOptionPane.showInputDialog("Por favor ingrese la opción que desee realizar\n"+
+                            "1.) Mostrar en forma 1\n"+
+              			  	"2.) Mostrar en forma 2\n"+
+              			  	"3.) Mostrar en forma 3\n");
+                	switch(subMenu){
+            			case "1":
+            				//Forma1 f1 = new Forma1();
+            				//res = f1.setVf1(result[1]);;
+            				break;
+            			case "2":
+            				//Forma2 f2 = new Forma2();
+            				//res = f2.setVf2(result[1]);;
+            				break;
+            			case "3":
+            				//Forma3 f3 = new Forma3();
+            				//res = f3.setVf3(result[1]);;
+            				break;
+            			default:
+            				JOptionPane.showMessageDialog(null, "Opción no válida", "Proyecto Polinomios", 0);
+            				break;
+                	}
                 case "3":
+                	termino = JOptionPane.showInputDialog("Ingrese el valor de X \n");
+                	EvaluarSegunX x = new EvaluarSegunX();
                 break; 
                 case "4":
+                	subMenu = JOptionPane.showInputDialog("Por favor ingrese la forma en la que desea sumar los dos polinomios\n"+
+                            "1.) Forma 1\n"+
+              			  	"2.) Forma 2\n"+
+              			  	"3.) Forma 3\n");
+                	switch(subMenu){
+            			case "1":
+            				break;
+            			case "2":
+            				break;
+            			case "3":
+            				break;
+            			default:
+            				JOptionPane.showMessageDialog(null, "Opción no válida", "Proyecto Polinomios", 0);
+            				break;
+                	}
                 case "5":
+                	subMenu = JOptionPane.showInputDialog("Por favor ingrese la forma en la que desea multiplicar los dos polinomios\n"+
+                            "1.) Forma 1\n"+
+              			  	"2.) Forma 2\n"+
+              			  	"3.) Forma 3\n");
+                	switch(subMenu){
+            			case "1":
+            				break;
+            			case "2":
+            				break;
+            			case "3":
+            				break;
+            			default:
+            				JOptionPane.showMessageDialog(null, "Opción no válida", "Proyecto Polinomios", 0);
+            				break;
+                	}
                 case "6":
                 case "7":
                 case "8":
